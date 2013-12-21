9d552ee5dcddaef79586bc8598ad1c18a5ceceda
SevenBits <meokid2@yahoo.com>
2013-12-21 17:31:26 -0500

    Removed Linux Mint 14.1 from the distribution downloader list, as it is now two releases old.

--- Mac Linux USB Loader/AppDelegate.m
+++ Mac Linux USB Loader/AppDelegate.m
@@ -20,7 +20,6 @@ DistributionDownloader *distributionDownloader;
  */
 NSString *urlArray[] = {
     @"http://releases.ubuntu.com/saucy/ubuntu-13.10-desktop-amd64+mac.iso", // Ubuntu 13.10 for Mac
-    @"http://linuxfreedom.com/linuxmint/stable/14/linuxmint-14.1-cinnamon-dvd-64bit.iso", // Mint 14.1 US
     @"http://linuxfreedom.com/linuxmint/stable/15/linuxmint-15-cinnamon-dvd-64bit.iso", // Mint 15 US
     @"http://distro.ibiblio.org/zorin/6/zorin-os-6.3-core-64.iso", // Zorin OS 6.3 US
     @"http://downloads.sourceforge.net/project/elementaryos/stable/elementaryos-stable-amd64.20130810.iso?r=http%3A%2F%2Felementaryos.org%2F&ts=1381599543&use_mirror=hivelocity" // Elementary OS Luna
