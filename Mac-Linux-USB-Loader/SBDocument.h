3e28e1fed3dbb704c09c46e6a99fac3644c2cb9d
SevenBits <meokid2@yahoo.com>
2014-01-13 16:47:24 -0500

    Initial Commit

---
+++ Mac-Linux-USB-Loader/SBDocument.h
@@ -0,0 +1,13 @@
+//
+//  SBDocument.h
+//  Mac-Linux-USB-Loader
+//
+//  Created by Ryan Bowring on 1/13/14.
+//  Copyright (c) 2014 SevenBits. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SBDocument : NSDocument
+
+@end
---
+++ Mac-Linux-USB-Loader/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Mac-Linux-USB-Loader
+//
+//  Created by Ryan Bowring on 1/13/14.
+//  Copyright (c) 2014 SevenBits. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
