184b17dce8bfd64562ae0b83feffe1aac4c51951
SevenBits <meokid2@yahoo.com>
2014-01-22 12:46:06 -0500

    Added XIB files for the persistence manager.

---
+++ Mac-Linux-USB-Loader/SBPersistenceManagerWindowController.h
@@ -0,0 +1,13 @@
+//
+//  SBPersistenceManagerWindowController.h
+//  Mac Linux USB Loader
+//
+//  Created by Ryan Bowring on 1/22/14.
+//  Copyright (c) 2014 SevenBits. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SBPersistenceManagerWindowController : NSWindowController
+
+@end
---
+++ Mac-Linux-USB-Loader/SBPersistenceManagerWindowController.m
@@ -0,0 +1,33 @@
+//
+//  SBPersistenceManagerWindowController.m
+//  Mac Linux USB Loader
+//
+//  Created by Ryan Bowring on 1/22/14.
+//  Copyright (c) 2014 SevenBits. All rights reserved.
+//
+
+#import "SBPersistenceManagerWindowController.h"
+
+@interface SBPersistenceManagerWindowController ()
+
+@end
+
+@implementation SBPersistenceManagerWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
