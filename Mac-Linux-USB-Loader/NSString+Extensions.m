01c17e2dcf20207be9945cf13a1d2cd5142b56f9
SevenBits <meokid2@yahoo.com>
2014-05-09 17:53:58 -0400

    Fix heading of some files.

--- Mac-Linux-USB-Loader/NSString+Extensions.m
+++ Mac-Linux-USB-Loader/NSString+Extensions.m
@@ -2,7 +2,7 @@
 //  NSString+Extensions.m
 //  Mac Linux USB Loader
 //
-//  Created by Ryan Bowring on 3/13/14.
+//  Created by SevenBits on 3/13/14.
 //  Copyright (c) 2014 SevenBits. All rights reserved.
 //
 
