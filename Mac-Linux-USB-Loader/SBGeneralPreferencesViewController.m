a2a6e223fb43758224f0d4b3bf76e94954f2dd8b
SevenBits <meokid2@yahoo.com>
2014-11-08 16:15:50 -0500

    Show a warning dialog before caches are cleared.

--- Mac-Linux-USB-Loader/SBGeneralPreferencesViewController.m
+++ Mac-Linux-USB-Loader/SBGeneralPreferencesViewController.m
@@ -24,7 +24,13 @@
 }
 
 - (IBAction)clearCachesButtonPressed:(id)sender {
-	[(SBAppDelegate *)[NSApp delegate] purgeCachesAndOldFiles];
+	NSAlert *alert = [[NSAlert alloc] init];
+	[alert addButtonWithTitle:NSLocalizedString(@"Yes", nil)];
+	[alert addButtonWithTitle:NSLocalizedString(@"No", nil)];
+	[alert setMessageText:NSLocalizedString(@"Are you sure you want to clear caches?", nil)];
+	[alert setInformativeText:NSLocalizedString(@"This can be useful if you want to conserve your disk space, but it will delete all of your downloaded ISO files and reset your download mirrors. Is this okay?", nil)];
+	[alert setAlertStyle:NSWarningAlertStyle];
+	[alert beginSheetModalForWindow:[self.view window] modalDelegate:self didEndSelector:@selector(deleteCachesAlertSheetDidEnd:returnCode:contextInfo:) contextInfo:nil];
 }
 
 - (NSString *)identifier {
@@ -37,6 +43,12 @@
 
 - (NSString *)toolbarItemLabel {
 	return NSLocalizedString(@"General", nil);
+}
+
+- (void)deleteCachesAlertSheetDidEnd:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo {
+	if (returnCode == NSAlertFirstButtonReturn) {
+		[(SBAppDelegate *)[NSApp delegate] purgeCachesAndOldFiles];
+	}
 }
 
 @end
