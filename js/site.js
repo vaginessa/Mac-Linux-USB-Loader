a456d8d195a504303a10fa1fb48ca953a8852e85
SevenBits <meokid2@yahoo.com>
2014-11-23 18:15:05 -0500

    Don't include Google Analytics in the JS
    Load from the HTML

--- js/site.js
+++ js/site.js
@@ -14,11 +14,3 @@ $(document).ready(function() {
 	
 	$(".download-btn").attr("href", "javascript:void(0);");
 });
-
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
-var pageTracker = _gat._getTracker("UA-46715734-1");
-pageTracker._trackPageview();
-} catch(err) {}
