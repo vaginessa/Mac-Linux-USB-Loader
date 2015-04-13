e23c08bf979525741746dceb636cd3bdb94adc0e
SevenBits <meokid2@yahoo.com>
2015-04-13 17:31:07 -0400

    Track when the actual MLUL download link is clicked.

--- js/site.js
+++ js/site.js
@@ -3,14 +3,18 @@ $(document).ready(function() {
 	$("#advert").click(function() {
 		$("#advert").hide();
 	});
-	
+
 	$(".download-btn").click(function() {
 		$("#advert").show();
 	})
-	
+
 	$("#payByIpnForm").submit(function() {
 		_gat._getTracker("UA-46715734-1")._trackEvent("Events", "Donate Clicked");
 	});
-	
+
+	$("#downloadLink").submit(function() {
+		_gat._getTracker("UA-46715734-1")._trackEvent("Events", "Download MLUL");
+	});
+
 	$(".download-btn").attr("href", "javascript:void(0);");
 });
