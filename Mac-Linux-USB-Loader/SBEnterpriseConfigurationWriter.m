223b5f9ae46897f20ea5122449c42c400c713280
SevenBits <meokid2@yahoo.com>
2016-05-22 10:16:16 -0400

    Remove a redundant kernel parameter.

--- Mac-Linux-USB-Loader/SBEnterpriseConfigurationWriter.m
+++ Mac-Linux-USB-Loader/SBEnterpriseConfigurationWriter.m
@@ -106,7 +106,7 @@
 		} else if (family == SBDistributionKali) {
 			[string appendString:@"kernel /live/vmlinuz findiso=/efi/boot/boot.iso boot=live noconfig=sudo username=root hostname=kali\n"];
 		} else if (family == SBDistributionTails) {
-			[string appendString:@"kernel /live/vmlinuz findiso=/efi/boot/boot.iso boot=live config live-media=removable noprompt timezone=Etc/UTC block.events_dfl_poll_msecs=1000 splash nox11autologin module=Tails quiet splash\n"];
+			[string appendString:@"kernel /live/vmlinuz findiso=/efi/boot/boot.iso boot=live config live-media=removable noprompt timezone=Etc/UTC block.events_dfl_poll_msecs=1000 nox11autologin module=Tails quiet splash\n"];
 		} else if (family == SBDistributionDebian) {
 			[string appendString:@"kernel /live/vmlinuz findiso=/efi/boot/boot.iso boot=live config live-config quiet splash"];
 		}
