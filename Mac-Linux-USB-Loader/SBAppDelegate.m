83fc3aee1cf8562993fa8f1d848741b0cdc68707
SevenBits <meokid2@yahoo.com>
2016-07-04 21:51:17 -0400

    Update Enterprise to the beta version of 0.4.

--- Mac-Linux-USB-Loader/SBAppDelegate.m
+++ Mac-Linux-USB-Loader/SBAppDelegate.m
@@ -17,7 +17,7 @@
 
 #define SBClearAllMenuItemTag 552345
 
-const NSString *SBBundledEnterpriseVersionNumber = @"0.3.2";
+const NSString *SBBundledEnterpriseVersionNumber = @"0.4";
 
 @interface SBAppDelegate () {
 	SBUSBSetupWindowController *usbSetupWindowController;
